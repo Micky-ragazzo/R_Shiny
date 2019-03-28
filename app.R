615202e2f436ad1682825727c0c27494df87544e
Micky-ragazzo <w48778@student.wsiz.rzeszow.pl>
2019-03-28 20:51:01 +0100

    Last changes

--- app.R
+++ app.R
@@ -1,71 +1,91 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-library(ggplot2)
+
 library(shiny)
+library(ggplot2)
+library(DT)
+library(dplyr)
 
-# Define UI for application that draws a histogram
+n_total <- nrow(df)
+all_genres <- sort(unique(df$Genres))
+
+# Define UI for application that plots features of movies 
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Pulpit nawigacyjny. Dane: google_play_tore"),
-   
-   # Sidebar with a slider input for number of bins 
-#   sidebarLayout(
- #     sidebarPanel(
-  #       sliderInput("bins",
- #                    "Number of bins:",
- #                    min = 1,
-  #                   max = 50,
- #                    value = 30)
-  #    ),
-      sidebarLayout(
-        sidebarPanel(
-          sliderInput(inputId = "y",
-                      label = "Y-axis:",
-                      choices = c("Rating" = googleplaystore$Rating),
-                                  #"Rating", "Size" , "Category", "Installs", "Type", "Price", "Content.Rating", "Genres", "Last.Updated", "Current.Ver", "Current.Ver"), 
-                      selected = "Rating")
-        ),
-        selectInput(inputId = "x", 
-                    label = "X-axis:",
-                    choices = c(
-                      #"Rating", 
-                      "Size"  = googleplaystore$Size),
-                      #"Category", "Installs", "Type", "Price", "Content.Rating", "Genres", "Last.Updated", "Current.Ver", "Current.Ver"), 
-                    selected = "Size")
-      ),
+  
+  titlePanel("Dashboard of google play"),
+  
+  # Sidebar layout with a input and output definitions 
+  sidebarLayout(
+    
+    # Inputs
+    sidebarPanel(
       
-      # Show a plot of the generated distribution
-      mainPanel(
-#         plotOutput("distPlot")
-         plotOutput(outputId = "scatterplot")
-      )
-   )
+      # Select variable for y-axis
+      selectInput(inputId = "y", 
+                  label = "Y-axis:",
+                  choices = c("Rating", "Size" , "Category", "Installs", "Type", "Price", "Content.Rating", "Genres", "Last.Updated", "Current.Ver", "Current.Ver", "Android.Ver"), 
+                  selected = "Category"),
+      # Select variable for x-axis
+      selectInput(inputId = "x", 
+                  label = "X-axis:",
+                  choices = c("Rating", "Size" , "Category", "Installs", "Type", "Price", "Content.Rating", "Genres", "Last.Updated", "Current.Ver", "Current.Ver", "Android.Ver"), 
+                  selected = "Installs"),
+      selectInput(inputId = "z", 
+                  label = "Color: by",
+                  choices = c("Rating", "Size" , "Category", "Installs", "Type", "Price", "Content.Rating", "Genres", "Last.Updated", "Current.Ver", "Current.Ver", "Android.Ver"),
+                  selected = "Type"),
+      sliderInput(inputId = "alpha",
+                  label = "View Scale:",
+                  min = 0, max = 1,
+                  value = 0.1),
+      selectInput(inputId = "studio",
+                  label = "Select genres:",
+                  choices = all_genres,
+                  selected = "SOCIAL",
+                  multiple = TRUE),
+      
+      HTML(paste("Enter a value between 1 and", n_total)),
+      numericInput(inputId = "n",
+                   label = "Sample size:",
+                   value = 30,
+                   min = 1, max = n_total,
+                   step = 1)
+    ),
+    
+    # Outputs
+    mainPanel(
+      plotOutput(outputId = "scatterplot"),
+      plotOutput(outputId = "densityplot", height = 200),
+      DT::dataTableOutput(outputId = "df_table")
+    )
+  )
+)
 
 
-# Define server logic required to draw a histogram
+
+# Define server function required to create the scatterplot
 server <- function(input, output) {
-   
-#   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-#      x    <- faithful[, 2] 
-#      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-#      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-#   })
-   output$scatterplot <- renderPlot({
-     ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
-       geom_point()
-   })
+  
+  # Create scatterplot object the plotOutput function is expecting
+  output$scatterplot <- renderPlot({
+    ggplot(data = df, aes_string(x = input$x, y = input$y, 
+                                 color = input$z)) +
+      geom_point(alpha = input$alpha)
+  })
+  output$densityplot <- renderPlot({
+    ggplot(data = df, aes_string(x = input$x)) +
+      geom_density()
+  })
+  output$df_table <- DT::renderDataTable({
+    req(input$Genres)
+    #req(input$studio)
+    df_sample <- df %>%
+      sample_n(input$n, input$Genres) %>%
+      select(Price:Category)
+    DT::datatable(data = df_sample, 
+                  options = list(pageLength = 10), 
+                  rownames = FALSE)
+    
+  })
 }
 
-# Run the application 
+# Create a Shiny app object
 shinyApp(ui = ui, server = server)
-
